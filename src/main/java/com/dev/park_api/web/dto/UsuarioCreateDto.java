521e7aaa69d3da12e1f311738374c5c061e61ccb
mar1a-ed <medu.correa29@gmail.com>
2026-03-25 12:33:51 -0300

    bean validation usuarios

--- src/main/java/com/dev/park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/dev/park_api/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,8 @@
 package com.dev.park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -9,8 +12,12 @@ import lombok.*;
 @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "Formato do e-mail inválido")
     private String username;
 
+    @NotBlank
+    @Size(message = "Tamanho mínimo e máximo = 6", min = 6, max = 6)
     private String password;
 
 }
--- src/main/java/com/dev/park_api/web/dto/UsuarioSenhaDto.java
+++ src/main/java/com/dev/park_api/web/dto/UsuarioSenhaDto.java
@@ -1,5 +1,7 @@
 package com.dev.park_api.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -9,9 +11,15 @@ import lombok.*;
 @ToString
 public class UsuarioSenhaDto {
 
+    @NotBlank
+    @Size(message = "Tamanho mínimo e máximo = 6", min = 6, max = 6)
     private String senhaAtual;
 
+    @NotBlank
+    @Size(message = "Tamanho mínimo e máximo = 6", min = 6, max = 6)
     private String novaSenha;
 
+    @NotBlank
+    @Size(message = "Tamanho mínimo e máximo = 6", min = 6, max = 6)
     private String confirmaSenha;
 }
